56eb4b5aef92a0b3abbeb3ecab38aa76db718168
zmatthews33 <zmatt33@gmail.com>
2019-10-01 14:46:08 -0500

    got dropdown working but home link needs fixed

--- js/main.js
+++ js/main.js
@@ -81,92 +81,82 @@ $(document).ready(function() {
 			}
 		}
 	});
-	// -------   Mail Send ajax
+
+	(function($) {
+		$.fn.bekeyProgressbar = function(options) {
+			options = $.extend(
+				{
+					animate: true,
+					animateText: true
+				},
+				options
+			);
+
+			var $this = $(this);
+
+			var $progressBar = $this;
+			var $progressCount = $progressBar.find('.progressBar-percentage-count');
+			var $circle = $progressBar.find('.progressBar-circle');
+			var percentageProgress = $progressBar.attr('data-progress');
+			var percentageRemaining = 100 - percentageProgress;
+			var percentageText = $progressCount.parent().attr('data-progress');
+
+			var radius = $circle.attr('r');
+			var diameter = radius * 2;
+			var circumference = Math.round(Math.PI * diameter);
+
+			var percentage = (circumference * percentageRemaining) / 100;
+
+			$circle.css({
+				'stroke-dasharray': circumference,
+				'stroke-dashoffset': percentage
+			});
+
+			if (options.animate === true) {
+				$circle
+					.css({
+						'stroke-dashoffset': circumference
+					})
+					.animate(
+						{
+							'stroke-dashoffset': percentage
+						},
+						3000
+					);
+			}
+
+			if (options.animateText == true) {
+				$({ Counter: 0 }).animate(
+					{ Counter: percentageText },
+					{
+						duration: 3000,
+						step: function() {
+							$progressCount.text(Math.ceil(this.Counter) + '%');
+						}
+					}
+				);
+			} else {
+				$progressCount.text(percentageText + '%');
+			}
+		};
+	})(jQuery);
 
 	$(document).ready(function() {
-		var form = $('#myForm'); // contact form
-		var submit = $('.submit-btn'); // submit button
-		var alert = $('.alert'); // alert div for show alert message
-
-
-
-(function($) {
-	$.fn.bekeyProgressbar = function(options) {
-		options = $.extend(
-			{
-				animate: true,
-				animateText: true
-			},
-			options
-		);
-
-		var $this = $(this);
-
-		var $progressBar = $this;
-		var $progressCount = $progressBar.find('.progressBar-percentage-count');
-		var $circle = $progressBar.find('.progressBar-circle');
-		var percentageProgress = $progressBar.attr('data-progress');
-		var percentageRemaining = 100 - percentageProgress;
-		var percentageText = $progressCount.parent().attr('data-progress');
-
-
-		var radius = $circle.attr('r');
-		var diameter = radius * 2;
-		var circumference = Math.round(Math.PI * diameter);
-
-
-		var percentage = (circumference * percentageRemaining) / 100;
-
-		$circle.css({
-			'stroke-dasharray': circumference,
-			'stroke-dashoffset': percentage
+		$('.progressBar--animateNone').bekeyProgressbar({
+			animate: false,
+			animateText: false
 		});
 
+		$('.progressBar--animateCircle').bekeyProgressbar({
+			animate: true,
+			animateText: false
+		});
 
-		if (options.animate === true) {
-			$circle
-				.css({
-					'stroke-dashoffset': circumference
-				})
-				.animate(
-					{
-						'stroke-dashoffset': percentage
-					},
-					3000
-				);
-		}
+		$('.progressBar--animateText').bekeyProgressbar({
+			animate: false,
+			animateText: true
+		});
 
-		if (options.animateText == true) {
-			$({ Counter: 0 }).animate(
-				{ Counter: percentageText },
-				{
-					duration: 3000,
-					step: function() {
-						$progressCount.text(Math.ceil(this.Counter) + '%');
-					}
-				}
-			);
-		} else {
-			$progressCount.text(percentageText + '%');
-		}
-	};
-})(jQuery);
-
-$(document).ready(function() {
-	$('.progressBar--animateNone').bekeyProgressbar({
-		animate: false,
-		animateText: false
+		$('.progressBar--animateAll').bekeyProgressbar();
 	});
-
-	$('.progressBar--animateCircle').bekeyProgressbar({
-		animate: true,
-		animateText: false
-	});
-
-	$('.progressBar--animateText').bekeyProgressbar({
-		animate: false,
-		animateText: true
-	});
-
-	$('.progressBar--animateAll').bekeyProgressbar();
 });
